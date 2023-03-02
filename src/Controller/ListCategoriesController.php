7ef28234ac741d63e1d7f8abd0527983fc2ea2c8
Emil Grozdanov <e_grozdanov@abv.bg>
2023-03-02 09:45:53 +0000

    refactor: AbstractController removed as unnecessary, improving readability of ListCategoriesController

--- src/Controller/ListCategoriesController.php
+++ src/Controller/ListCategoriesController.php
@@ -6,20 +6,19 @@ use BlogPostsHandling\Api\Repository\CategoryRepositoryByPdo;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use function MongoDB\BSON\toJSON;
 
 class ListCategoriesController
 {
-
     public function __invoke(Request $request, Response $response, $args): Response
     {
-        $categoryRepo = new CategoryRepositoryByPdo();
-
-        if ( $categories = $categoryRepo->fetchAll() ) {
+        if ( $categories = (new CategoryRepositoryByPdo)->fetchAll() ) {
             return new JsonResponse([
                 "message" => "categories successfully retrieved",
                 "msgid" => "categories_retrieved",
-                "categories" => array_map(function($c){ return $c->toMap(); } , $categories)
+                "categories" => array_map(
+                                   function($c){ return $c->toMap(); } ,
+                                   $categories
+                                )
             ], 200);
 
         } else
